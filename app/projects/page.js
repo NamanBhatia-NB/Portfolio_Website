37fbd59ee2d67c3fb28c88eb68ab3e9c4573e833
NamanBhatia-NB <145786560+NamanBhatia-NB@users.noreply.github.com>
2024-10-11 03:13:34 +0530

    Update page.js

--- app/projects/page.js
+++ app/projects/page.js
@@ -73,7 +73,7 @@ const projects = [
 
 const Projects = () => {
   return (
-    <div className="container mx-auto px-4 pt-8 pb-28 ">
+    <div className="container mx-auto px-4 pt-8 pb-36 ">
       <h1 className="text-3xl font-bold mb-8 text-center">My Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-3">
         {projects.map((project) => (
